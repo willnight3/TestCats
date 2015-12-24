0878041b27c562bf4f4af25d55122f222569135a
Dasha <willnight@mail.ru>
2015-12-24 22:00:07 +0300

    Create MainActivity

---
+++ TestCatNew/app/src/main/java/com/testsut/testcatnew/MainActivity.java
@@ -0,0 +1,17 @@
+package com.testsut.testcatnew;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Mitrofanova on 23.12.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
